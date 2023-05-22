277d583cd53bb8b568b66e3a775e06e48f13d487
Melissa <mnjuarez@scl.edu.gt>
2023-05-22 17:58:34 -0600

    Actualización 2

--- js/main.js
+++ js/main.js
@@ -3,9 +3,7 @@
     //Se llama a la función que inserta las teclas
     cargar_teclas();
 
-
-
-    let totales = document.querySelector("#total"); //total tiene un valor input
+   /* let totales = document.querySelector("#total"); //total tiene un valor input
 
     function agregar(){
         let caracter = this.innerHTML;
@@ -56,3 +54,73 @@
             keys_content.addEventListener("click", agregar);
         }
     });
+*/
+
+let totales = document.querySelector("#total"); //total tiene un valor input
+
+    function agregar(){
+        let caracter = this.innerHTML;
+        totales = document.querySelector("#total");
+        totales.value = totales.value + caracter;
+    }
+
+    function reinicio(){
+        totales = document.querySelector("#total");
+        totales.value = "";
+    }
+
+    function reinicio_total(){
+        let totales_a = document.querySelector("#total");
+        totales_a.value = 0;
+        document.querySelector(".record").innerHTML = "";
+    }
+
+    function borrar(){
+        var contenido = totales.value;
+        var nuevoValor = contenido.slice(0, -1);
+        totales.value = nuevoValor;
+    }
+
+    function calculate() {
+    let operation = document.querySelector("#total");
+    let i = eval(operation.value);
+    set_record(operation.value, i);
+    operation.value = r;
+    actualizarResultados();
+    }
+
+    //updateCurrentResult();
+    function actualizarResultados() {
+    let records_box_DOM = document.querySelector(".record");
+    let resultSpans = records_box_DOM.querySelectorAll(".result");
+    let UltimoResultadoSpan = resultSpans[resultSpans.length - 1];
+
+    if (UltimoResultadoSpan) {
+        let result = UltimoResultadoSpan.innerText;
+        //let operation = UltimoResultadoSpan.previousSibling.innerText;
+
+        document.querySelector("#total").value = result;
+    } else {
+        document.querySelector("#total").value = "";
+    }
+    }
+
+    
+    let keys_dom = document.querySelectorAll(".key_item");
+
+    keys_dom.forEach((keys_content) => {
+    let content = keys_content.innerHTML;
+
+    if (content == "C") {
+        keys_content.addEventListener("click", reinicio);
+    } else if (content == "=") {
+        keys_content.addEventListener("click", calculate);
+    } else if (content == "AC") {
+        keys_content.addEventListener("click", reinicio_total);
+    } else if (content == '<img src="./img/delete.png" class="delete">') {
+        keys_content.addEventListener("click", borrar);
+    } else {
+        keys_content.addEventListener("click", agregar);
+    }
+    });
+
--- js/resultados.js
+++ js/resultados.js
@@ -1,5 +1,5 @@
 //records_components.js
-function set_record(operation, result){
+/*function set_record(operation, result){
     let item_record = operation + "=\t" + result;
     let records_box_DOM = document.querySelector(".record");
     records_box_DOM.innerHTML += `
@@ -10,3 +10,89 @@ function set_record(operation, result){
         </li>`;
     records_box_DOM.scrollTop = records_box_DOM.scrollHeight;
 }
+*/
+
+function set_record(operation, result) {
+    let item_record = operation + "=\t" + result;
+    let records_box_DOM = document.querySelector(".record");
+  
+    let listItem = document.createElement("li");
+
+    let operationSpan = document.createElement("span");
+    operationSpan.innerText = operation;
+
+    let equalSignSpan = document.createElement("span");
+    equalSignSpan.classList.add("equal-sign");
+    equalSignSpan.innerText = "=";
+
+    let resultSpan = document.createElement("span");
+    resultSpan.classList.add("result");
+    resultSpan.innerText = result;
+  
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-btn");
+    deleteButton.innerText = "X";
+
+    deleteButton.addEventListener("click", function () {
+    listItem.remove();
+    updateCurrentResult();
+
+    });
+  
+    listItem.appendChild(operationSpan);
+    listItem.appendChild(equalSignSpan);
+    listItem.appendChild(resultSpan);
+    listItem.appendChild(deleteButton);
+  
+    records_box_DOM.appendChild(listItem);
+    records_box_DOM.scrollTop = records_box_DOM.scrollHeight;
+  }
+  
+  function updateCurrentResult() {
+    let records_box_DOM = document.querySelector(".record");
+    let resultSpans = records_box_DOM.querySelectorAll(".result");
+    let lastResultSpan = resultSpans[resultSpans.length - 1];
+  
+    if (lastResultSpan) {
+      let result = lastResultSpan.innerText;
+      let operation = lastResultSpan.previousSibling.innerText;
+  
+      document.querySelector("#total").value = result;
+    } else {
+      document.querySelector("#total").value = "";
+    }
+  }
+
+
+    document.getElementById("download-btn").addEventListener("click", function() { downloadHistorial(); });
+  
+
+
+  function downloadHistorial() {
+    let records_box_DOM = document.querySelector(".record");
+    let resultSpans = records_box_DOM.querySelectorAll(".result");
+    let operationSpans = records_box_DOM.querySelectorAll("span:not(.result):not(.delete-btn)");
+  
+
+    let historial = "";
+    for (let i = 0; i < resultSpans.length; i++) {
+      let operation = operationSpans[i].innerText.trim(); //trim elimina los espacios en blanco 
+      let result = resultSpans[i].innerText.trim();
+      historial += operation + " = " + result + "\n";
+    }
+  
+    // Crear un elemento de enlace temporal
+    const link = document.createElement("a");
+    link.href = "data:text/plain;charset=utf-8," + encodeURIComponent(historial);
+    link.download = "historial.txt";
+    link.style.display = "none";
+    
+  
+    // Agregar el enlace al documento y simular un click para descargar el archivo
+    document.body.appendChild(link);
+    link.click();
+  
+    // Eliminar el enlace del documento
+    document.body.removeChild(link);
+  }
+  
